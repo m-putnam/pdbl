f51eedbc8348dbd8efddbe55d8e2ea7358fff489
Matthew Lavin <pbtflakes@protonmail.ch>
2017-04-30 16:10:08 -0400

    additional progress

--- pdbl.scm
+++ pdbl.scm
@@ -4,45 +4,49 @@
 ;; Copyright 2017 Matthew Lavin
 ;;
 ;; This program utilizes the lalr-scm parser generation library, which is free
-;; software licensed under the GNU LGPL.
+;; software licensed under the GNU LGPL. Development and testing is done using
+;; GNU Guile, so interoperability cannot be guaranteed.
 ;;
 
 (load "lalr-scm/lalr.scm")
 
 (define *tape* #())	; Tape operated on by the machine
-(define *pos* 0)	; Current position of tape head
+(define *position* 0)	; Current position of tape head
 (define *state* 's1)	; Current state of machine
 
 (define (setup tape pos state)
   (set! *tape* tape)
-  (set! *pos* pos)
+  (set! *position* pos)
   (set! *state* state))
 
 (define (set-state! state)
   (set! *state* state))
 
 (define (set-pos! pos)
-  (set! *pos* pos))
+  (set! *position* pos))
 
 (define (movl)
-  (set! *pos* (- *pos* 1)))
+  (set! *position* (- *position* 1)))
 
 (define (movr)
-  (set! *pos* (+ *pos* 1)))
+  (set! *position* (+ *position* 1)))
 
 (define (write out)
-  (vector-set! *tape* *pos* out))
+  (vector-set! *tape* *position* out))
 
 (define (read)
-  (vector-ref *tape* *pos*))
+  (vector-ref *tape* *position*))
 
 (define (neq a b)
   (not (= a b)))
 
 ; Increment square & move head left
-(define (lambda-operator)
+(define (Lambda)
   (write (+ (read) 1))
   (movl))
+
+(define (Alpha)
+  (eq? (read) '_))
 
 (define (while-tape func)
   (while (neq (read) 0)
@@ -50,13 +54,6 @@
 
 ; Some example input to work with
 (define tape1
-  (list->vector '(1 1 1 1 0 _ _ _)))
+  (list->vector '(0 1 1 1 1 0 _ _ _)))
 
 (setup tape1 0 's1)
-
-(newline)
-(do ()
-  ((not (= (read) 1)))
-  (format #t "~d" (read))
-  (newline)
-  (movr))
